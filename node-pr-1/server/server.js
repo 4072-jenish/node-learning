402dba99bbf56a207fb12bb4a6e243b3f127ade3
malaviyapunam <159758998+malaviyapunam@users.noreply.github.com>
2025-07-15 09:35:45 +0530

    project-1 update

---
+++ node-pr-1/server/server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  let filename = "";
+  switch (req.url) {
+    case "/":
+      filename = "../index.html";
+      break;
+    case "/about":
+      filename = "../about.html";
+      break;
+    case "/services":
+      filename = "../services.html";
+      break;
+    case "/contact": 
+      filename = "../contact.html";
+      break;
+    default:
+      filename = "../404.html";
+      break;
+  }
+
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404 not found");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    }
+  });
+});
+
+server.listen(8088, () => {
+  console.log("Server running at http://localhost:8088");
+});
