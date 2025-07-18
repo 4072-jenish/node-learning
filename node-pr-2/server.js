4db29e0c60a418850a6cf163f6ff7146390811da
malaviyapunam <159758998+malaviyapunam@users.noreply.github.com>
2025-07-18 12:10:01 +0530

    notification update

--- node-pr-2/server.js
+++ node-pr-2/server.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: 'todo-secret',
+    resave: false,
+    saveUninitialized: true
+}));
+
 let todos = [
     { title: 'Buy Groceries', content: 'Milk, Bread, Eggs' },
     { title: 'Study', content: 'Read Express.js documentation' },
     { title: 'Exercise', content: '30-minute run in the evening' }
 ];
+
+app.use((req, res, next) => {
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
+});
 
 app.get('/', (req, res) => {
     res.render('index', { todos });
@@ -20,6 +33,7 @@ app.post('/add', (req, res) => {
     const { title, content } = req.body;
     if (title && content) {
         todos.push({ title, content });
+        req.session.message = 'Todo added successfully!';
     }
     res.redirect('/');
 });
@@ -28,6 +42,7 @@ app.post('/delete', (req, res) => {
     const index = req.body.index;
     if (index !== undefined) {
         todos.splice(index, 1);
+        req.session.message = 'Todo deleted!';
     }
     res.redirect('/');
 });
@@ -47,6 +62,7 @@ app.post('/edit', (req, res) => {
     if (todos[index]) {
         todos[index].title = title;
         todos[index].content = content;
+        req.session.message = 'Todo updated!';
     }
     res.redirect('/');
 });
