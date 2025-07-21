ce014b6c6ca116008acf03fdb2bd7e650a899792
malaviyapunam <159758998+malaviyapunam@users.noreply.github.com>
2025-07-21 10:31:22 +0530

    FINAL UPDATED

--- node-pr-2/server.js
+++ node-pr-2/server.js
@@ -1,39 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const session = require('express-session');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(session({
-    secret: 'todo-secret',
-    resave: false,
-    saveUninitialized: true
-}));
 
 let todos = [
-    { title: 'Buy Groceries', content: 'Milk, Bread, Eggs' },
-    { title: 'Study', content: 'Read Express.js documentation' },
-    { title: 'Exercise', content: '30-minute run in the evening' }
+    { title: 'Buy Groceries', content: 'Milk, Bread, jam', priority: 'One', date: '2025-07-22' },
+    { title: 'Study', content: 'Read Express.js documentation', priority: 'Two', date: '2025-07-23' },
+    { title: 'Exercise', content: '30-minute run in the evening', priority: 'Three', date: '2025-07-24' }
 ];
 
-app.use((req, res, next) => {
-    res.locals.message = req.session.message;
-    delete req.session.message;
-    next();
-});
-
 app.get('/', (req, res) => {
-    res.render('index', { todos });
+    res.render('index', { todos, message: null });
 });
 
 app.post('/add', (req, res) => {
-    const { title, content } = req.body;
-    if (title && content) {
-        todos.push({ title, content });
-        req.session.message = 'Todo added successfully!';
+    const { title, content, priority, date } = req.body;
+    if (title && content && priority && date) {
+        todos.push({ title, content, priority, date });
     }
     res.redirect('/');
 });
@@ -42,7 +27,6 @@ app.post('/delete', (req, res) => {
     const index = req.body.index;
     if (index !== undefined) {
         todos.splice(index, 1);
-        req.session.message = 'Todo deleted!';
     }
     res.redirect('/');
 });
@@ -58,15 +42,16 @@ app.get('/edit/:index', (req, res) => {
 });
 
 app.post('/edit', (req, res) => {
-    const { index, title, content } = req.body;
+    const { index, title, content, priority, date } = req.body;
     if (todos[index]) {
         todos[index].title = title;
         todos[index].content = content;
-        req.session.message = 'Todo updated!';
+        todos[index].priority = priority;
+        todos[index].date = date;
     }
     res.redirect('/');
 });
 
 app.listen(8000, () => {
-    console.log('Server is running on http://localhost:8000');
+    console.log('Server is running at http://localhost:8000');
 });
